cf7807b289c43e6e2f5504031a48b4a0ea02ebb7
Rob Tuley <rob@openknot.com>
2016-04-05 21:19:46 +0100

    Added client port from env to allow circleci tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,11 @@ echo ${MYID:-1} > /opt/zookeeper/data/myid
 if [ -n "$SERVERS" ]; then
 	IFS=\, read -a servers <<<"$SERVERS"
 	for i in "${!servers[@]}"; do 
-		printf "\nserver.%i=%s:2888:3888" "$((1 + $i))" "${servers[$i]}" >> /opt/zookeeper/conf/zoo.cfg
+		printf "\nserver.%i=%s" "$((1 + $i))" "${servers[$i]}" >> /opt/zookeeper/conf/zoo.cfg
 	done
 fi
+
+printf "\nclientPort=%i" "${CLIENT_PORT:-2181}" >> /opt/zookeeper/conf/zoo.cfg
 
 cd /opt/zookeeper
 exec "$@"

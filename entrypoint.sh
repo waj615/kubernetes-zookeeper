dc1a899de391abf5f87535fc3b91dce859159609
Rob Tuley <rob@openknot.com>
2016-04-24 22:42:35 +0100

    Modified entrypoint to use kubernetes service environment vars when available

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 echo ${MYID:-1} > /opt/zookeeper/data/myid
 
+# SERVERS and CLIENT_PORT inputs exist for test and other uses outside kubernetes of this container. 
+
 if [ -n "$SERVERS" ]; then
 	IFS=\, read -a servers <<<"$SERVERS"
 	for i in "${!servers[@]}"; do 
@@ -10,6 +12,18 @@ if [ -n "$SERVERS" ]; then
 fi
 
 printf "\nclientPort=%i" "${CLIENT_PORT:-2181}" >> /opt/zookeeper/conf/zoo.cfg
+
+# Within kubernetes, we rely on the service environment variables.
+
+for i in 1 2 3
+do
+	host="ZOOKEEPER_${i}_SERVICE_HOST"
+	followers="ZOOKEEPER_${i}_SERVICE_PORT_FOLLOWERS"
+	election="ZOOKEEPER_${i}_SERVICE_PORT_ELECTION"
+	if [ -n "${!host}" ]; then
+		printf "\nserver.%i=%s" "$i" "${!host}:${!followers}:${!election}" >> /opt/zookeeper/conf/zoo.cfg
+	fi
+done
 
 cd /opt/zookeeper
 exec "$@"

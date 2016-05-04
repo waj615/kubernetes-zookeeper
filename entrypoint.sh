8faa606f19d46d9731e4c1e10deb964980c8c6eb
Rob Tuley <rob@openknot.com>
2016-05-04 12:34:00 +0100

    Moved monitoring code first in entrypoint to remove need for data dir init

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-echo ${MYID:-1} > /opt/zookeeper/data/myid
-
-client="ZOOKEEPER_${MYID}_SERVICE_PORT_CLIENT"
-printf "\nclientPort=%i" "${!client:-2181}" >> /opt/zookeeper/conf/zoo.cfg
-
-for i in 1 2 3 4 5
-do
-	host="ZOOKEEPER_${i}_SERVICE_HOST"
-	followers="ZOOKEEPER_${i}_SERVICE_PORT_FOLLOWERS"
-	election="ZOOKEEPER_${i}_SERVICE_PORT_ELECTION"
-	if [ -n "${!host}" ]; then
-		printf "\nserver.%i=%s" "$i" "${!host}:${!followers}:${!election}" >> /opt/zookeeper/conf/zoo.cfg
-	fi
-done
-
 if [ "$MYID" == "monitor" ]; then
 	while :
 	do
@@ -32,6 +17,21 @@ if [ "$MYID" == "monitor" ]; then
 		done
 	done
 fi
+
+echo ${MYID:-1} > /opt/zookeeper/data/myid
+
+client="ZOOKEEPER_${MYID}_SERVICE_PORT_CLIENT"
+printf "\nclientPort=%i" "${!client:-2181}" >> /opt/zookeeper/conf/zoo.cfg
+
+for i in 1 2 3 4 5
+do
+	host="ZOOKEEPER_${i}_SERVICE_HOST"
+	followers="ZOOKEEPER_${i}_SERVICE_PORT_FOLLOWERS"
+	election="ZOOKEEPER_${i}_SERVICE_PORT_ELECTION"
+	if [ -n "${!host}" ]; then
+		printf "\nserver.%i=%s" "$i" "${!host}:${!followers}:${!election}" >> /opt/zookeeper/conf/zoo.cfg
+	fi
+done
 
 cd /opt/zookeeper
 exec "$@"
